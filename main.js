88b3f3d9c0cbc3042d6b9b570bc9386e40d602af
ashok70775 <ashok70775@gmail.com>
2025-05-19 19:56:32 +0530

    setting up connection to mongodb

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 import express from "express";
 import moviesRoutes from "./routes/movies.routes.js";
+import connectDB  from "./lib/db.js";  
 
 
 const app = express();
 const port = 3300;
 
+//to get the MongoDB connection
+connectDB();
+
 app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
+
+
 
 
 // Middleware for the routes
