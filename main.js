78b264975683eba13b300d536983f180e10eff0d
ashok70775 <ashok70775@gmail.com>
2025-05-19 17:21:55 +0530

    route structure

--- main.js
+++ main.js
@@ -7,6 +7,23 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello World" });
 });
 
+app.get("/movies",()=>{
+
+
+});
+
+app.post("/movies",()=>{
+
+})
+
+app.put("/movies:id",()=>{
+
+})
+
+app.delete("/movies:id",()=>{})
+
+
+
 app.listen(port, () => {
   console.log(`Server is running on port  http://localhost:${port}`);
 });
